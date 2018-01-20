1a812e129fa776aa6df2f0018c8000531871a1bd
nomad1072 <siddharthlanka@gmail.com>
2018-01-21 02:29:06 +0530

    Small UI fixes/ Styling React Modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,10 +3,15 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => {
   return (
-    <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option" onRequestClose={props.handleSelectedOption}>
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.handleSelectedOption}>Okay</button>
+    <Modal isOpen={!!props.selectedOption}
+      contentLabel="Selected Option"
+      onRequestClose={props.handleSelectedOption}
+      closeTimeout={200}
+      className="modal"
+    >
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+      <button className="button" onClick={props.handleSelectedOption}>Okay</button>
     </Modal>
   );
 };
